af616896740cd5db6899ab6a9b209e91961c9112
LucasGabriel09 <164903588+LucasGabriel09@users.noreply.github.com>
2025-06-02 12:29:39 -0300

    Create questaoum.c

---
+++ questaoum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+int a,b;
+printf("Digite o primeiro número: ");
+  scanf("%d", &a);
+printf("Digite o segundo número: ");
+  scanf("%d", &b);
+printf("O valor da soma dos números é: %d", a+b);
+}
