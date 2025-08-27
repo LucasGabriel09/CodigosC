de863562f53f77f0b281d09772a877000e810967
LucasGabriel09 <164903588+LucasGabriel09@users.noreply.github.com>
2025-08-27 12:56:53 -0300

    Questao64matriz.c

--- Questao64matriz.c
+++ Questao64matriz.c
@@ -1,21 +1,16 @@
+#include <stdio.h>
+int main(void) {
+int num[3][3];
+printf("Digite os números da matriz: ");
+    for(int i = 0; i < 3; i++) {
+    for(int j = 0; j < 3; j++) {
+    scanf("%d", &num[i][j]); 
+}
+    }
+    printf("\nDiagonal principal:\n");
+    for (int i = 0; i < 3; i++) {
+        printf("%d ", num[i][i]);
+    }
 
- #include <stdio.h>
-
-    int main(void) {
-    int num[3][3];
-    printf("Digite os números: ");
-    for(int i=0; i < 3; i++) {
-     for(int j=0; j < 3; j++){
-        scanf("%d", &num[i][j]);
-    }
-    } 
-        printf("Números digitados: ");
-        for(int i=0; i < 3; i++) {
-         for(int j=0; j < 3; j++){
-            printf("%d", num[i][j]);
-        
-    
-    }
-        }
-        return 0;
-    }
+    printf("\n");
+}
